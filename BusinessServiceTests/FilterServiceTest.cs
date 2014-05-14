2da76e94fba91ce63d0f7d6379c34e8474cecb81
Khashayar <khparhami@gmail.com>
2014-05-14 14:30:02 +1000

    Test Added

---
+++ BusinessServiceTests/FilterServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessService;
+using Domain.Models;
+
+namespace BusinessServiceTests
+{
+    [TestClass]
+    public class FilterServiceTest
+    {
+        [TestMethod]
+        public void FilterShows_Always_ReturnAnArrayOfShows()
+        {
+            //Arrange
+            var target = new FilterService();
+
+            //Action
+
+            var actual = target.FilterShows(new Request { payload = new[] { new Payload { country = "UK" } } });
+
+            //Assert
+
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual,typeof(Show[]));
+        }
+    }
+}
